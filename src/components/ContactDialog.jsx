8d07e3c18082824e397aea8f7ace66f0ff14cfe0
cacheUp <bradb26@gmail.com>
2019-02-05 11:45:03 +0300

    ui for edit and delete to show up if it is the post belongs to current user

---
+++ src/components/ContactDialog.jsx
@@ -0,0 +1,98 @@
+/* eslint-disable no-useless-constructor */
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import Avatar from "@material-ui/core/Avatar";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemAvatar from "@material-ui/core/ListItemAvatar";
+import ListItemText from "@material-ui/core/ListItemText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import Dialog from "@material-ui/core/Dialog";
+import PersonIcon from "@material-ui/icons/Person";
+import AddIcon from "@material-ui/icons/Add";
+import Typography from "@material-ui/core/Typography";
+import blue from "@material-ui/core/colors/blue";
+import Fab from "@material-ui/core/Fab";
+
+const styles = {
+  avatar: {
+    backgroundColor: blue[100],
+    color: blue[600]
+  }
+};
+
+const ContactDialog = props => {
+  const [open, setOpen] = useState(false);
+  const { classes } = props;
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = value => {
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <Fab
+        color="primary"
+        aria-label="Add"
+        className={`${classes.fab} prison-fab-button`}
+        onClick={handleClickOpen}
+        size="medium"
+      >
+        <AddIcon />
+      </Fab>
+      <Dialog aria-labelledby="simple-dialog-title" open={open}>
+        <DialogTitle id="simple-dialog-title">Contact Info</DialogTitle>
+        <div>
+          <List>
+            <ListItem>
+              <ListItemAvatar>
+                <Avatar className={classes.avatar}>
+                  <img
+                    src={require("./Images/jobtitle.png")}
+                    alt="Position:"
+                    className="contact-image1"
+                  />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText primary={props.prisonContact.contactName} />
+            </ListItem>
+
+            <ListItem>
+              <ListItemAvatar>
+                <Avatar className={classes.avatar}>
+                  <img
+                    src={require("./Images/phone.jpeg")}
+                    alt="Position:"
+                    className="contact-image2"
+                  />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText primary={props.prisonContact.phoneNumber} />
+            </ListItem>
+
+            <ListItem>
+              <ListItemAvatar>
+                <Avatar className={classes.avatar}>
+                  <img
+                    src={require("./Images/email.jpeg")}
+                    alt="Email:"
+                    className="contact-image3"
+                  />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText primary={props.prisonContact.contactEmail} />
+            </ListItem>
+          </List>
+        </div>
+      </Dialog>
+    </div>
+  );
+};
+
+export default ContactDialog;
--- src/components/PrisonPost.jsx
+++ src/components/PrisonPost.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+/**@jsx jsx */
+import React, { useContext } from "react";
 import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
@@ -10,6 +11,9 @@ import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import CardHeader from "@material-ui/core/CardHeader";
 import "./PrisonPosts.css";
+import ContactDialog from "./ContactDialog";
+import { UserContext } from "../providers/UserProvider";
+import { jsx, css } from "@emotion/core";
 
 const styles = {
   card: {
@@ -26,62 +30,97 @@ const styles = {
 };
 
 function PrisonPost(props) {
+  const currentUser = useContext(UserContext);
+  const user = props.user;
+  const belongsToCurrentUser = (currentUser, postAuthor) => {
+    if (!currentUser) return false;
+    return currentUser.uid === postAuthor.uid;
+  };
+
+  const marginStyle =
+    !belongsToCurrentUser(currentUser, user) &&
+    css`
+      margin-top: 25px;
+    `;
+
   const { classes } = props;
   return (
-    <Card className={classes.card}>
-      <div>
-        <CardHeader
-          component="p"
-          id="prison-post-header"
-          color="inherit"
-          align="center"
-          title={props.prisonName}
-        >
-          <p>hey</p>
-        </CardHeader>
-      </div>
-      <CardContent>
-        <div className="card-container">
-          <div className="mid-container">
-            <div className="address-container">
-              {" "}
-              <Typography gutterBottom variant="p" component="p">
-                <span>{props.prisonAddress.street}</span>{" "}
-                <span>
-                  <br />
-                  {props.prisonAddress.city},<span> </span>
-                </span>
-                <span>{props.prisonAddress.pState} </span>
-                <span>{props.prisonAddress.zip}</span>
-              </Typography>
-              <div className="available-workers">
-                <Typography component="p">Available Workers: 1</Typography>
-              </div>
-              <div>
-                <ContactDialog {...props} />
-              </div>
-            </div>
-            <div className="view-profile-container">
-              <CardMedia
-                component="img"
-                alt="profile icon"
-                className={classes.media}
-                image={require("./Images/p.png")}
-              />
-              <div className="space" />
-              <Button
-                color="primary"
-                variant="raised"
-                size="small"
-                className="btn"
-              >
-                View Profile
-              </Button>
-            </div>
+    <div css={marginStyle}>
+      {belongsToCurrentUser(currentUser, user) && (
+        <div className="edit-delete-container">
+          <div className="edit-wrapper">
+            <span className="edit-icon">edit</span>
+            <img src={require("./Images/pedit.png")} alt="edit" />
+          </div>
+          <div className="edit-wrapper">
+            <span className="edit-icon">delete</span>
+            <img src={require("./Images/delete.png")} alt="delete" />
           </div>
         </div>
-      </CardContent>
-    </Card>
+      )}
+      <Card className={classes.card}>
+        <div>
+          <CardHeader
+            component="p"
+            id="prison-post-header"
+            color="inherit"
+            align="center"
+            title={props.prisonName}
+          />
+        </div>
+        <CardContent>
+          <div className="card-container">
+            <div className="mid-container">
+              <div className="address-container">
+                {" "}
+                <Typography gutterBottom variant="p" component="p">
+                  <span className="address-text">
+                    {props.prisonAddress.street}
+                  </span>{" "}
+                  <span className="address-text">
+                    <br />
+                    {props.prisonAddress.city},<span> </span>
+                  </span>
+                  <span className="address-text">
+                    {props.prisonAddress.pState}{" "}
+                  </span>
+                  <span className="address-text">
+                    {props.prisonAddress.zip}
+                  </span>
+                </Typography>
+                <div className="available-workers">
+                  <Typography component="p">Available Workers: 1</Typography>
+                </div>
+              </div>
+              <div className="view-profile-container">
+                <CardMedia
+                  component="img"
+                  alt="profile icon"
+                  className={classes.media}
+                  image={require("./Images/p.png")}
+                />
+                <div className="space" />
+                <Button
+                  color="primary"
+                  variant="raised"
+                  size="small"
+                  className="btn"
+                >
+                  View Profile
+                </Button>
+              </div>
+            </div>
+            <div className="contact-info-container">
+              <span className="span-container">
+                <span className="contact-info-header">Contact Info</span>
+
+                <ContactDialog {...props} />
+              </span>
+            </div>
+          </div>
+        </CardContent>
+      </Card>
+    </div>
   );
 }
 
